0263bd1c071ae74a8f37df97ac34711424946173
DimaKadich <LedvNeon@rambler.ru>
2024-10-10 22:03:56 +0300

    Отредактировал скрипт натсройки Ansible

--- ansible.sh
+++ ansible.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum -y install epel-release
-sudo yum -y install ansible
+sudo yum update -y #обновим ОС
+sudo yum -y install epel-release #добавим репозиторий epel-release
+sudo yum -y install ansible #установим ansible
+#создадим inventory-файл
 cat <<EOF> /etc/ansible/hosts
 [web]
 10.200.1.3
 EOF
+#обавим шлюз по-умолчанию
+route add default gw 10.200.1.1
+#выключим лишние интерфейсы
+ifconfig eth0 down
+ifconfig eth2 down
