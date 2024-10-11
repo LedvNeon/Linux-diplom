efa5cd1b68050e27a332ec1dac5753bb96b12aa4
DimaKadich <LedvNeon@rambler.ru>
2024-10-11 20:45:22 +0300

    ansible.sh настройки для ansible.cfg

--- ansible.sh
+++ ansible.sh
@@ -3,6 +3,7 @@ yum update -y #обновим ОС
 yum -y install epel-release #добавим репозиторий epel-release
 yum install -y python3
 yum -y install ansible #установим ansible
+
 #создадим inventory-файл
 cat <<EOF> /etc/ansible/hosts
 [web]
@@ -10,16 +11,21 @@ webdmz ansible_host=10.200.1.3
 [web:vars}
 ansible_user=vagrant
 EOF
+
 # изменим права (так делаем только в рамка хтестовой среды)
 chmod 777 /etc
 chmod 777 /etc/ansible
 # создадим дирректорию для playbooks
 sudo mkdir /etc/ansible/playbooks
 chmod 777 /etc/ansible/playbooks
+
+# Внесём изменения в конфиг для ansible, что бы playbook 
+# не запрашивал подтверждения на доверия хосту при подключении
 cat <<EOF> /etc/ansible/ansible.cfg
 [defaults]
 host_key_checking = False
 EOF
+
 #обавим шлюз по-умолчанию
 route add default gw 10.200.1.1
 #выключим лишние интерфейсы
