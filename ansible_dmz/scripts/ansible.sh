7b9c3834480238968945d7f17d22beb8c8a23baa
DimaKadich <LedvNeon@rambler.ru>
2024-10-22 17:34:07 +0300

    папка с конфигами для ansibledmz

---
+++ ansible_dmz/scripts/ansible.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+yum update -y #обновим ОС
+yum -y install epel-release #добавим репозиторий epel-release
+yum install -y python3
+yum -y install ansible #установим ansible
+
+#создадим inventory-файл
+cat <<EOF> /etc/ansible/hosts
+[web]
+webdmz2 ansible_host=10.200.1.4
+[web:vars}
+ansible_user=vagrant
+EOF
+
+# изменим права (так делаем только в рамка хтестовой среды)
+chmod 777 /etc
+chmod 777 /etc/ansible
+# создадим дирректорию для playbooks
+sudo mkdir /etc/ansible/playbooks
+chmod 777 /etc/ansible/playbooks
+
+# Внесём изменения в конфиг для ansible, что бы playbook 
+# не запрашивал подтверждения на доверия хосту при подключении
+cat <<EOF> /etc/ansible/ansible.cfg
+[defaults]
+host_key_checking = False
+EOF
+
+#обавим шлюз по-умолчанию
+route add default gw 10.200.1.1
+#выключим лишние интерфейсы
+#ifconfig eth0 down
+#ifconfig eth2 down
+
+mkdir /etc/ansible/files
+chmod 777 /etc/ansible/files
