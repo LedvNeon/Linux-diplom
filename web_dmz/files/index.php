6af1e8c4eb22012f0b3b1ce309c7c8155d555c63
DimaKadich <LedvNeon@rambler.ru>
2024-10-23 19:54:31 +0300

    Добавил webdmz (может что то менял)

--- web_dmz/files/index.php
+++ web_dmz/files/index.php
@@ -1,5 +1,5 @@
 <?php
-$db = pg_connect('host=127.0.0.1 port=5432 dbname=test_db user=tuser password=super_str0ng_pa$$word');
+$db = pg_connect('host=127.0.0.1 port=5432 dbname=test_db user=tuser password=PassW0rdStr0ng');
 $result = pg_query($db, "select * from test_tb");
 $resultArr = pg_fetch_all($result);
 print_r($resultArr);
