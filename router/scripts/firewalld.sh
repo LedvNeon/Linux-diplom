05d77fc46037337d23f4dce6e175fcb576b2d154
DimaKadich <LedvNeon@rambler.ru>
2025-01-24 13:06:49 +0300

    Доделал репликацию, актуализировал схему сети

--- router/scripts/firewalld.sh
+++ router/scripts/firewalld.sh
@@ -8,6 +8,7 @@ firewall-cmd --zone=dmz --add-port=22/tcp --permanent  # откроем порт
 firewall-cmd --zone=public --add-port=8000/tcp --permanent # откроем порт 8000 на public для http
 firewall-cmd --zone=public --add-port=2443/tcp --permanent  # откроем порт 2443 на public для https
 firewall-cmd --zone=public --add-port=2222/tcp --permanent  # откроем порт 2222 на public для ssh
+firewall-cmd --zone=public --add-port=4444/tcp --permanent
 firewall-cmd --direct --permanent --add-rule ipv4 filter FORWARD 0 -i eth1 -o eth3 -j ACCEPT # разрешим трафик от интерфейса eth1 к eth3
 firewall-cmd --direct --permanent --add-rule ipv4 filter FORWARD 0 -i eth3 -o eth1 -j ACCEPT # разрешим трафик от интерфейса eth3 к eth1
 firewall-cmd --zone=public --add-masquerade --permanent # включим NAT на public
@@ -23,6 +24,7 @@ firewall-cmd --zone=public --add-forward-port=port=9090:proto=tcp:toport=9090:to
 firewall-cmd --zone=public --add-forward-port=port=9113:proto=tcp:toport=9113:toaddr=10.200.1.4 --permanent
 firewall-cmd --zone=public --add-forward-port=port=8001:proto=tcp:toport=8001:toaddr=10.200.1.4 --permanent
 firewall-cmd --zone=public --add-forward-port=port=2244:proto=tcp:toport=22:toaddr=10.200.1.7 --permanent
+firewall-cmd --zone=public --add-forward-port=port=4444:proto=tcp:toport=22:toaddr=10.200.1.6 --permanent
 firewall-cmd --zone=dmz --add-forward-port=port=1111:proto=tcp:toport=22:toaddr=172.16.1.5 --permanent
 firewall-cmd --zone=dmz --add-forward-port=port=5514:proto=tcp:toport=514:toaddr=172.16.1.5 --permanent
 firewall-cmd --zone=dmz --add-forward-port=port=5514:proto=udp:toport=514:toaddr=172.16.1.5 --permanent
